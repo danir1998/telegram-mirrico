955d71fe0c5ee338b0b22a7bfa2126397dafaf0a
danirka <danir12345@gmail.com>
2020-01-18 20:20:38 +0300

    change

--- bot.php
+++ bot.php
@@ -6,12 +6,9 @@ require_once("vendor/autoload.php");
 try {
     $bot = new \TelegramBot\Api\Client('1011193242:AAFhi5OqpF92E3YC3j8KwIcCOh7k-HyF5yY');
 
-
-
-
     $bot->command('start', function ($message) use ($bot) {
         $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array(array("one", "two", "three")), true); // true for one-time keyboard
-        $answer = 'Добро пожаловать! Ильдар';
+        $answer = json_encode($message->getChat());
         $bot->sendMessage($message->getChat()->getId(), $answer, null, false, null, $keyboard);
     });
 
