05006163846faf9b1bf7f1fc83a9570a2420ae0f
danirka <danir12345@gmail.com>
2020-01-18 20:29:01 +0300

    sdas

--- bot.php
+++ bot.php
@@ -7,7 +7,14 @@ try {
     $bot = new \TelegramBot\Api\Client('1011193242:AAFhi5OqpF92E3YC3j8KwIcCOh7k-HyF5yY');
 
     $bot->command('start', function ($message) use ($bot) {
-        $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array(array("one", "two", "three")), true); // true for one-time keyboard
+        $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(
+            [
+                ['7', '8', '9'],
+                ['4', '5', '6'],
+                ['1', '2', '3'],
+                [     '0'     ]
+            ]
+        , true); // true for one-time keyboard
         $answer = json_encode($message->getChat());
         $bot->sendMessage($message->getChat()->getId(), $answer, null, false, null, $keyboard);
     });
