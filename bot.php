9b1b566c3845adffcba6fa9cb4e97decc5e4bfbb
danirka <danir12345@gmail.com>
2020-01-19 18:50:15 +0300

    change

--- bot.php
+++ bot.php
@@ -40,7 +40,9 @@ try {
     $bot->on(function($update) use ($bot, $callback_loc, $find_command){
         $callback = $update->getCallbackQuery();
         $message = $callback->getMessage();
+
         $mtext = $update->getMessage()->getText();
+
         $chatId = $message->getChat()->getId();
         $data = $callback->getData();
 
@@ -53,7 +55,7 @@ try {
         }
         if($mtext == "Сиськи 👋"){
             $pic = "http://aftamat4ik.ru/wp-content/uploads/2017/05/14277366494961.jpg";
-            $bot->sendPhoto($update->getChat()->getMessage()->getId(), $pic);
+            $bot->sendPhoto($update->getMessage()->getChat()->getId(), $pic);
         }
 
     }, function($update){
